24283b562758b6dd7e452004d095473460c45d82
Chan0129 <142906874+Chan0129@users.noreply.github.com>
2024-08-30 22:10:41 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    container: {
+      screens: {
+        sm: "375px",
+        md: "768px",
+        lg: "1440px",
+      },
+    },
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1440px",
+    },
+    extend: {
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 1s ease-in-out",
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
+};
